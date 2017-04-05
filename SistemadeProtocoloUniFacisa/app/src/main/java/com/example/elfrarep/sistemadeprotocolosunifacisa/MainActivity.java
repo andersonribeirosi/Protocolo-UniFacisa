5a02b608e8544c201125a66441224a4ac4bbb5b1
Anderson Ribeiro <andersonribeiro.si@hotmail.com>
2017-04-05 02:00:32 -0300

    App Android Sistema de Protocolos da UniFacisa

---
+++ SistemadeProtocoloUniFacisa/app/src/main/java/com/example/elfrarep/sistemadeprotocolosunifacisa/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.elfrarep.sistemadeprotocolosunifacisa;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    //Ação dos botões
+    public void cadastro (View v){
+        setContentView(R.layout.cadastro);
+    }
+
+    public void acompanharProtocolo (View v){
+        setContentView(R.layout.acompanhar_protocolo);
+    }
+
+    public void pesquisarProtocolo (View v){
+        setContentView(R.layout.pesquisar_protocolo);
+    }
+
+    public void solicitarProtocolo (View v){
+        setContentView(R.layout.solicitar_protocolo);
+    }
+
+    public void voltar (View v){
+        setContentView(R.layout.activity_main);
+    }
+
+    public void fechar (View v){
+        finish();
+    }
+
+    public void enviarFormulario(View view){
+        FrameLayout fl = (FrameLayout) findViewById(R.id.fl);
+        fl.setVisibility(View.VISIBLE);
+    }
+}
+
